c5adfd2025f6cd1fa05bfe5af32ec7b4c228f9b2
Santos Vilanculos <14965649-SantosVilanculos@users.noreply.gitlab.com>
2024-09-11 13:03:55 +0200

    style: change variable name

--- src/main.py
+++ src/main.py
@@ -8,13 +8,13 @@ from window import Window
 
 
 def main() -> None:
-    PATH = contents_directory_path()
+    cdp = contents_directory_path()
 
     application = QApplication(sys.argv)
-    QFontDatabase.addApplicationFont(str(PATH.joinpath("Inter-Regular.ttf")))
-    QFontDatabase.addApplicationFont(str(PATH.joinpath("Inter-Medium.ttf")))
-    QFontDatabase.addApplicationFont(str(PATH.joinpath("Inter-Bold.ttf")))
-    QFontDatabase.addApplicationFont(str(PATH.joinpath("JetBrainsMono-Regular.ttf")))
+    QFontDatabase.addApplicationFont(str(cdp.joinpath("Inter-Regular.ttf")))
+    QFontDatabase.addApplicationFont(str(cdp.joinpath("Inter-Medium.ttf")))
+    QFontDatabase.addApplicationFont(str(cdp.joinpath("Inter-Bold.ttf")))
+    QFontDatabase.addApplicationFont(str(cdp.joinpath("JetBrainsMono-Regular.ttf")))
     application.setFont(QFont("Inter", int(14 / (96 / 72)), 400))
 
     window = Window()
