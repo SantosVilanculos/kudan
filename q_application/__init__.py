3ebe2938c27061f3ee893f769a60b477afc31723
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-11-13 18:21:53 +0200

    refactor: separate the application logic

---
+++ q_application/__init__.py
@@ -0,0 +1,81 @@
+import sys
+from functools import partial
+
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QAction, QFontDatabase, QIcon
+from PySide6.QtWidgets import QApplication, QMainWindow, QMenu, QSystemTrayIcon
+
+from environment import APPLICATION_NAME, contents_path
+from q_application.central_widget import CentralWidget
+from q_application.q_font import q_font
+from q_application.q_palette import q_palette
+
+
+def q_action_1_triggered(q_action: QAction, q_main_window: QMainWindow) -> None:
+    q_main_window.setVisible(not q_main_window.isVisible())
+
+    if q_main_window.isVisible():
+        q_action.setText("Hide")
+    else:
+        q_action.setText("Show")
+
+
+def q_application():
+    cp = contents_path()
+
+    _ = QApplication(sys.argv)
+
+    _.setStyle("Fusion")
+
+    _.setPalette(q_palette)
+
+    QFontDatabase.addApplicationFont(str(cp.joinpath("inter/4.0/inter-bold.ttf")))
+    QFontDatabase.addApplicationFont(str(cp.joinpath("inter/4.0/inter-medium.ttf")))
+    QFontDatabase.addApplicationFont(str(cp.joinpath("inter/4.0/inter-regular.ttf")))
+    _.setFont(q_font)
+
+    q_icon = QIcon(str(cp.joinpath("favicon.ico")))
+
+    q_main_window = QMainWindow()
+    q_main_window.setWindowIcon(q_icon)
+    q_main_window.setWindowTitle(APPLICATION_NAME)
+    q_main_window.setMinimumSize(QSize(640, 360))
+
+    central_widget = CentralWidget()
+    q_main_window.setCentralWidget(central_widget)
+
+    if QSystemTrayIcon.isSystemTrayAvailable():
+        q_system_tray_icon = QSystemTrayIcon(q_main_window)
+
+        q_system_tray_icon.setIcon(q_icon)
+
+        q_system_tray_icon.activated.connect(
+            lambda activation_reason: q_main_window.setVisible(
+                not q_main_window.isVisible()
+            )
+        )
+
+        q_menu = QMenu()
+        q_font.setPixelSize(12)
+        q_menu.setFont(q_font)
+
+        q_action_1 = QAction("Hide")
+        q_action_1.triggered.connect(
+            partial(q_action_1_triggered, q_action_1, q_main_window)
+        )
+        q_main_window.showEvent = lambda q_show_event: q_action_1.setText("Hide")
+        q_main_window.hideEvent = lambda q_hide_event: q_action_1.setText("Show")
+        q_menu.addAction(q_action_1)
+
+        # q_menu.addSeparator()
+
+        q_action_2 = QAction("Exit")
+        q_action_2.triggered.connect(_.quit)
+        q_menu.addAction(q_action_2)
+
+        q_system_tray_icon.setContextMenu(q_menu)
+        q_system_tray_icon.show()
+        _.setQuitOnLastWindowClosed(not q_system_tray_icon.isVisible())
+
+    q_main_window.show()
+    sys.exit(_.exec())
---
+++ q_application/q_font.py
@@ -0,0 +1,6 @@
+from PySide6.QtGui import QFont
+
+q_font = QFont("Inter")
+q_font.setPixelSize(13)
+q_font.setWeight(QFont.Weight.Normal)
+q_font.setStyleStrategy(QFont.StyleStrategy.PreferQuality)
