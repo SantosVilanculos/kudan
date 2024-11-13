dc3358ff808c67b47c88fa5966430f4f11fb45eb
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-11-13 19:44:25 +0200

    chore: update the process table in real time

--- screen/process_iter.py
+++ screen/process_iter.py
@@ -1,6 +1,6 @@
-from psutil import Process, process_iter
-from PySide6.QtCore import QMargins
-from PySide6.QtGui import Qt
+from psutil import AccessDenied, NoSuchProcess, Process, process_iter
+from PySide6.QtCore import QMargins, QTimer
+from PySide6.QtGui import QHideEvent, QShowEvent, Qt
 from PySide6.QtWidgets import (
     QAbstractItemView,
     QHeaderView,
@@ -41,11 +41,61 @@ class Widget(QWidget):
         self.q_table_widget.setContextMenuPolicy(
             Qt.ContextMenuPolicy.ActionsContextMenu
         )
-        for index, process in enumerate(process_iter()):
-            self.q_table_widget_insert_row(index, process)
         self.q_table_widget.setSortingEnabled(True)
         self.q_table_widget.sortByColumn(0, Qt.SortOrder.AscendingOrder)
         self.q_v_box_layout.addWidget(self.q_table_widget)
+
+        self.q_timer = QTimer(self)
+        self.q_timer.setInterval(1_000)
+        self.q_timer.timeout.connect(self.q_timer_timeout)
+
+    def q_timer_timeout(self) -> None:
+        process_list: list[Process] = list(process_iter())
+        if not process_list:
+            return
+
+        process_list_pid: list[int] = [int(process.pid) for process in process_list]
+
+        # Create a set of PIDs from the q_table_widget
+        q_table_widget_pids = set(
+            int(self.q_table_widget.item(index, 0).text())
+            for index in range(self.q_table_widget.rowCount())
+        )
+
+        # Remove the processes from the process_list that are already present in the q_table_widget
+        process_list = [
+            process
+            for process in process_list
+            if int(process.pid) not in q_table_widget_pids
+        ]
+
+        # Remove the rows from the q_table_widget that are no longer in the process_list
+        for index in reversed(range(self.q_table_widget.rowCount())):
+            pid = int(self.q_table_widget.item(index, 0).text())
+            if pid not in process_list_pid:
+                self.q_table_widget.removeRow(index)
+
+        # Add the remaining processes to the q_table_widget
+        row_count = self.q_table_widget.rowCount()
+        for index, process in enumerate(process_list):
+            try:
+                if Process(process.pid).is_running():
+                    self.q_table_widget_insert_row(
+                        row=(row_count + index), process=process
+                    )
+            except (NoSuchProcess, AccessDenied):
+                # TODO:log it
+                # Skip the process if it no longer exists or we don't have permission to access it
+                pass
+
+    def showEvent(self, event: QShowEvent) -> None:
+        self.q_timer_timeout()
+        self.q_timer.start()
+        return super().showEvent(event)
+
+    def hideEvent(self, event: QHideEvent) -> None:
+        self.q_timer.stop()
+        return super().hideEvent(event)
 
     def q_table_widget_insert_row(self, row: int, process: Process) -> None:
         # process_iter.clear_cache()
