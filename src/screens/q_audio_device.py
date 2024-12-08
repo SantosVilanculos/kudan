04ae3410137b4f2a38493e3b8e657b101ed7f68f
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-12-08 17:41:51 +0200

    refactor: use one card and add id field

--- src/screens/q_audio_device.py
+++ src/screens/q_audio_device.py
@@ -14,7 +14,7 @@ from PySide6.QtWidgets import (
 from components.Page import Page
 
 
-class CardInput(QWidget):
+class Card(QWidget):
     def __init__(self, device: QAudioDevice):
         super().__init__()
         self.device = device
@@ -36,6 +36,10 @@ class CardInput(QWidget):
 
         q_form_layout = QFormLayout()
         q_form_layout.setVerticalSpacing(12)
+
+        self.id = QLabel(str(device.id()))
+        self.id.setAlignment(Qt.AlignmentFlag.AlignRight)
+        q_form_layout.addRow("id", self.id)
 
         self.description = QLabel(device.description())
         self.description.setAlignment(Qt.AlignmentFlag.AlignRight)
@@ -62,6 +66,7 @@ class CardInput(QWidget):
         q_form_layout.addRow("maximum_channel_count", self.maximum_channel_count)
 
         self.minimum_sample_rate = QLabel(f"{device.minimumSampleRate()}Hz (Hertz)")
+
         self.minimum_sample_rate.setAlignment(Qt.AlignmentFlag.AlignRight)
         q_form_layout.addRow("minimum_sample_rate", self.minimum_sample_rate)
 
@@ -92,6 +97,9 @@ class CardInput(QWidget):
             self.q_widget.setStyleSheet(
                 "#form{border:1px solid #e0e0e0;background-color:#ffffff}"
             )
+
+        self.id.setText(str(device.id()))
+
         self.description.setText(device.description())
 
         self.mode.setText(device.mode().name)
@@ -116,11 +124,11 @@ class CardInput(QWidget):
 class ListInput(QWidget):
     def __init__(self):
         super().__init__()
-        self.cards: list[CardInput] = list([])
+        self.cards: list[Card] = list([])
         self.q_v_box_layout = QVBoxLayout(self)
         self.q_v_box_layout.setContentsMargins(0, 0, 0, 0)
         for device in QMediaDevices.audioInputs():
-            card = CardInput(device)
+            card = Card(device)
             self.cards.append(card)
             self.q_v_box_layout.addWidget(card, alignment=Qt.AlignmentFlag.AlignTop)
 
@@ -144,7 +152,7 @@ class ListInput(QWidget):
 
             if existing_card is None:
                 # Create a new card if no card exists for this device
-                new_card = CardInput(device)
+                new_card = Card(device)
                 self.q_v_box_layout.addWidget(
                     new_card, alignment=Qt.AlignmentFlag.AlignTop
                 )
@@ -163,114 +171,14 @@ class ListInput(QWidget):
         return super().hideEvent(event)
 
 
-class CardOutput(QWidget):
-    def __init__(self, device: QAudioDevice):
-        super().__init__()
-        self.device = device
-
-        q_stacked_layout = QStackedLayout(self)
-        self.q_widget = QWidget()
-        self.q_widget.setObjectName("form")
-        if device.isDefault():
-            self.q_widget.setStyleSheet(
-                "#form{border:1px solid #34a853;background-color:#ffffff}"
-            )
-        else:
-            self.q_widget.setStyleSheet(
-                "#form{border:1px solid #e0e0e0;background-color:#ffffff}"
-            )
-        q_v_box_layout = QVBoxLayout(self.q_widget)
-        q_v_box_layout.setContentsMargins(24, 24, 24, 24)
-        q_v_box_layout.setSpacing(24)
-
-        q_form_layout = QFormLayout()
-        q_form_layout.setVerticalSpacing(12)
-
-        self.description = QLabel(device.description())
-        self.description.setAlignment(Qt.AlignmentFlag.AlignRight)
-        q_form_layout.addRow("description", self.description)
-
-        self.mode = QLabel(device.mode().name)
-        self.mode.setAlignment(Qt.AlignmentFlag.AlignRight)
-        q_form_layout.addRow("mode", self.mode)
-
-        self.channel_configuration = QLabel(device.channelConfiguration().name)
-        self.channel_configuration.setAlignment(Qt.AlignmentFlag.AlignRight)
-        q_form_layout.addRow("channel_configuration", self.channel_configuration)
-
-        self.is_default = QLabel(str(device.isDefault()))
-        self.is_default.setAlignment(Qt.AlignmentFlag.AlignRight)
-        q_form_layout.addRow("is_default", self.is_default)
-
-        self.minimum_channel_count = QLabel(str(device.minimumChannelCount()))
-        self.minimum_channel_count.setAlignment(Qt.AlignmentFlag.AlignRight)
-        q_form_layout.addRow("minimum_channel_count", self.minimum_channel_count)
-
-        self.maximum_channel_count = QLabel(str(device.maximumChannelCount()))
-        self.maximum_channel_count.setAlignment(Qt.AlignmentFlag.AlignRight)
-        q_form_layout.addRow("maximum_channel_count", self.maximum_channel_count)
-
-        self.minimum_sample_rate = QLabel(f"{device.minimumSampleRate()}Hz (Hertz)")
-
-        self.minimum_sample_rate.setAlignment(Qt.AlignmentFlag.AlignRight)
-        q_form_layout.addRow("minimum_sample_rate", self.minimum_sample_rate)
-
-        self.maximum_sample_rate = QLabel(f"{device.maximumSampleRate()}Hz (Hertz)")
-        self.maximum_sample_rate.setAlignment(Qt.AlignmentFlag.AlignRight)
-        q_form_layout.addRow("maximum_sample_rate", self.maximum_sample_rate)
-
-        self.preferred_format = QLabel(str(device.preferredFormat()))
-        self.preferred_format.setAlignment(Qt.AlignmentFlag.AlignRight)
-        q_form_layout.addRow("preferred_format", self.preferred_format)
-
-        self.supported_sample_formats = QLabel(str(device.supportedSampleFormats()))
-        self.supported_sample_formats.setAlignment(Qt.AlignmentFlag.AlignRight)
-        q_form_layout.addRow("supported_sample_formats", self.supported_sample_formats)
-
-        q_v_box_layout.addLayout(q_form_layout)
-        q_stacked_layout.addWidget(self.q_widget)
-
-    def systemId(self) -> int:
-        return self.device.id()
-
-    def update(self, device: QAudioDevice):
-        if device.isDefault():
-            self.q_widget.setStyleSheet(
-                "#form{border:1px solid #34a853;background-color:#ffffff}"
-            )
-        else:
-            self.q_widget.setStyleSheet(
-                "#form{border:1px solid #e0e0e0;background-color:#ffffff}"
-            )
-        self.description.setText(device.description())
-
-        self.mode.setText(device.mode().name)
-
-        self.channel_configuration.setText(device.channelConfiguration().name)
-
-        self.is_default.setText(str(device.isDefault()))
-
-        self.minimum_channel_count.setText(str(device.minimumChannelCount()))
-
-        self.maximum_channel_count.setText(str(device.maximumChannelCount()))
-
-        self.minimum_sample_rate.setText(f"{device.minimumSampleRate()}Hz (Hertz)")
-
-        self.maximum_sample_rate.setText(f"{device.maximumSampleRate()}Hz (Hertz)")
-
-        self.preferred_format.setText(str(device.preferredFormat()))
-
-        self.supported_sample_formats.setText(str(device.supportedSampleFormats()))
-
-
 class ListOutput(QWidget):
     def __init__(self):
         super().__init__()
-        self.cards: list[CardInput] = list([])
+        self.cards: list[Card] = list([])
         self.q_v_box_layout = QVBoxLayout(self)
         self.q_v_box_layout.setContentsMargins(0, 0, 0, 0)
         for device in QMediaDevices.audioOutputs():
-            card = CardInput(device)
+            card = Card(device)
             self.cards.append(card)
             self.q_v_box_layout.addWidget(card, alignment=Qt.AlignmentFlag.AlignTop)
 
@@ -294,7 +202,7 @@ class ListOutput(QWidget):
 
             if existing_card is None:
                 # Create a new card if no card exists for this device
-                new_card = CardOutput(device)
+                new_card = Card(device)
                 self.q_v_box_layout.addWidget(
                     new_card, alignment=Qt.AlignmentFlag.AlignTop
                 )
