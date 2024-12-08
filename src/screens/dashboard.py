2e2fa931c8e13ce7a0821b66d4755ab6047ba7f6
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2024-12-08 18:52:07 +0200

    chore: move close button to menu

--- src/screens/dashboard.py
+++ src/screens/dashboard.py
@@ -1,16 +1,14 @@
-from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QPushButton, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QStackedLayout, QVBoxLayout, QWidget
 
 
 class Widget(QWidget):
     def __init__(self) -> None:
         super().__init__()
 
-        q_v_box_layout = QVBoxLayout(self)
+        q_stacked_layout = QStackedLayout(self)
+        q_widget = QWidget()
+        q_widget.setStyleSheet("background-color:#cc0000")
+        q_v_box_layout = QVBoxLayout(q_widget)
         q_v_box_layout.setContentsMargins(0, 0, 0, 0)
         q_v_box_layout.setSpacing(0)
-        q_v_box_layout.setAlignment(Qt.AlignmentFlag.AlignTop)
-
-        q_push_button = QPushButton("...")
-        q_push_button.clicked.connect(QApplication.quit)
-        q_v_box_layout.addWidget(q_push_button)
+        q_stacked_layout.addWidget(q_widget)
