dc7b30f7f9548aab204b56053794929eb891fa61
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-05-13 01:43:45 +0200

    chore: update logger configuration

--- src/logger.py
+++ src/logger.py
@@ -1,25 +1,43 @@
-from logging import DEBUG, INFO, Logger, StreamHandler, basicConfig, getLogger
-from logging.handlers import RotatingFileHandler
-
-
-from custom.contents_path import contents_path
-
-path = contents_path(".log")
-# path.mkdir(parents=True, exist_ok=True)
-
-rotating_file_handler = RotatingFileHandler(
-    filename=path,
-    maxBytes=512,
-    backupCount=2,
+import sys
+from logging import (
+    DEBUG,
+    Handler,
+    Logger,
+    StreamHandler,
+    basicConfig,
+    getLogger,
 )
-rotating_file_handler.setLevel(INFO)
+from logging.handlers import RotatingFileHandler
+from pathlib import Path
+from typing import List
+
+from PySide6.QtCore import QStandardPaths
+
+from environment import APP_NAME
+
+_handlers: List[Handler] = []
+
+if not getattr(sys, "frozen", False):
+    _handlers.append(StreamHandler())
+
+_app_data_path = Path(
+    QStandardPaths.writableLocation(
+        QStandardPaths.StandardLocation.GenericDataLocation
+    ),
+    APP_NAME,
+).resolve()
+_app_data_path.mkdir(parents=True, exist_ok=True)
+
+_handlers.append(
+    RotatingFileHandler(filename=_app_data_path.joinpath(".log"), maxBytes=512)
+)
 
 basicConfig(
     force=True,
     level=DEBUG,
     encoding="UTF-8",
     format="%(asctime)s:%(levelname)s:%(filename)s:%(lineno)d:%(message)s",
-    handlers=[StreamHandler(), rotating_file_handler],
+    handlers=_handlers,
 )
 
 logger: Logger = getLogger()
